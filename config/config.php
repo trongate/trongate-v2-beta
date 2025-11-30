fca12825e4ed8afb536861a9b982fbb35b3127c8
David Connelly <davidc.webguy@gmail.com>
2025-11-30 13:50:43 +0000

    slightly lost

--- config/config.php
+++ config/config.php
@@ -6,6 +6,8 @@ define('DEFAULT_MODULE', 'welcome');
 define('DEFAULT_METHOD', 'index');
 define('MODULE_ASSETS_TRIGGER', '_module');
 define('ERROR_404', 'templates/error_404');
+define('FLASHDATA_OPEN', '<p style="color: green;">');
+define('FLASHDATA_CLOSE', '</p>');
 
 $interceptors = [
     'endpoint_listener' => 'record'
